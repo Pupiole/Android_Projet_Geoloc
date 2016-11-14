1ce18e839cdd1d77eb54cb1f306afaaaef0a8fd9
Yiserot <yserot.perso@gmail.com>
2016-11-14 15:53:09 +0100

    Android project creation
    #GEO-110

---
+++ Geoquest/app/src/main/java/fr/enssat/serot_ldp/geoquest/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.enssat.serot_ldp.geoquest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
