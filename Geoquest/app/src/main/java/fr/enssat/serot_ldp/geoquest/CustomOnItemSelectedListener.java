851de6f762076a6758cb6992ed35d7920bcd493b
LOUIS DIT PICARD <paulldp@hotmail.fr>
2016-12-01 10:16:57 +0100

    Menu & Spinner
    #GEO-30

---
+++ Geoquest/app/src/main/java/fr/enssat/serot_ldp/geoquest/CustomOnItemSelectedListener.java
@@ -0,0 +1,18 @@
+package fr.enssat.serot_ldp.geoquest;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+/**
+ * Created by paul on 01/12/16.
+ */
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
---
+++ Geoquest/app/src/main/java/fr/enssat/serot_ldp/geoquest/Menu.java
@@ -0,0 +1,65 @@
+package fr.enssat.serot_ldp.geoquest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by paul on 01/12/16.
+ */
+
+public class Menu extends AppCompatActivity {
+
+    private Spinner spinner;
+    private Button btnjouer, btnediter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addItemsOnSpinner();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+    }
+
+
+
+    public void addItemsOnSpinner() {
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    public void addListenerOnButton(){
+
+        btnjouer = (Button) findViewById(R.id.button4);
+        btnediter = (Button) findViewById(R.id.button6);
+
+        spinner.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu.this,"OnClickListener : Spinner 1 : "+ String.valueOf(spinner.getSelectedItem()), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void addListenerOnSpinnerItemSelection(){
+
+        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+}
