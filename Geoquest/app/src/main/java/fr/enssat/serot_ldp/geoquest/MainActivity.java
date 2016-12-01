a94cd43ff1deed58dce1fb727af732ac8c3c0502
LOUIS DIT PICARD <paulldp@hotmail.fr>
2016-12-01 08:49:16 +0100

    maj gitignore
    #GEO-100

--- Geoquest/app/src/main/java/fr/enssat/serot_ldp/geoquest/MainActivity.java
+++ Geoquest/app/src/main/java/fr/enssat/serot_ldp/geoquest/MainActivity.java
@@ -2,6 +2,7 @@ package fr.enssat.serot_ldp.geoquest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.*;
 
 public class MainActivity extends AppCompatActivity {
 
